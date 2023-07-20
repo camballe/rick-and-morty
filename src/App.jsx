ac17f7e1ba59e411175adbf2016ae8b48995bca5
Enoch Kambale <campnoch@gmail.com>
2023-07-20 15:05:04 +0300

    finished app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Characters from "./components/Characters";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Rick and Morty</h1>
+        <QueryClientProvider client={queryClient}>
+          <Characters />
+        </QueryClientProvider>
+      </div>
+    </div>
+  );
 }
 
 export default App;
